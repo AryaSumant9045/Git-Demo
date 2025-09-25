70f31ccc5067a694cfbfe0ffd48dd9f0775c7eb2
Arya Sumant <studywithpwno.1@gmail.com>
2025-09-25 20:38:54 +0530

    auto update 2025-09-25 20:38:54

--- auto_push.sh
+++ auto_push.sh
@@ -1,5 +1,15 @@
+# #!/bin/bash
+# cd ~/Desktop/git-demo   # apna repo ka path
+# git add .
+# git commit -m "Auto commit: $(date)"
+# git push origin main
+
 #!/bin/bash
-cd ~/Desktop/git-demo   # apna repo ka path
-git add .
-git commit -m "Auto commit: $(date)"
-git push origin main
+
+while true
+do
+  git add .
+  git commit -m "auto update $(date '+%Y-%m-%d %H:%M:%S')"
+  git push origin main
+  sleep 30   # har 1 minute me push karega
+done
